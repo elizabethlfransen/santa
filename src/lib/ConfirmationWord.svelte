0a0551f8eb0e6b59b79e061e9cafb7ba80b13f98
Liz Fransen <lfransen@sourceallies.com>
2023-11-02 10:18:55 -0500

    Utilize Routing

---
+++ src/lib/ConfirmationWord.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {CONFIRMATION_WORD} from "$lib/constants"
+</script>
+<span>
+    {CONFIRMATION_WORD}
+</span>
+
+<style lang="scss">
+  span {
+    text-transform: uppercase;
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    color: #aaa;
+    font-size: 1.5rem;
+    font-family: sans-serif;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,67 @@
 <script>
-    import NameSelectPage from "$lib/NameSelectPage.svelte";
-    import ResultPage from "$lib/ResultPage.svelte";
-    import {MATCHES, CONFIRMATION_WORD} from "$lib/constants"
+    import ConfirmationWord from "$lib/ConfirmationWord.svelte";
+    import "@fontsource/dancing-script"
+    import NameSelect from "$lib/NameSelect.svelte";
 
-    /** @type {string} */
-    let name = "";
-
-    function handle(data) {
-        name = data.detail.name;
+    function handleSubmit(e) {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        window.location.href += encodeURIComponent(data.get("name").toString());
+        return false;
     }
 </script>
-<span>
-    {CONFIRMATION_WORD}
-</span>
-{#if name === ""}
-    <NameSelectPage on:change={handle}/>
-{:else}
-    <ResultPage match={MATCHES[name]}/>
-{/if}
+<ConfirmationWord/>
+<form on:submit={handleSubmit} class="outer">
+    <div>
+        <label for="name">Select Your Name</label>
+        <div class="inner">
+            <NameSelect name="name"/>
+            <button type="submit">go</button>
+        </div>
+    </div>
+</form>
+
 <style lang="scss">
-    span {
-      text-transform: uppercase;
-      position: absolute;
-      top: 1rem;
-      right: 1rem;
-      color: #aaa;
-      font-size: 1.5rem;
-      font-family: sans-serif;
+  button {
+    height: 4.4em;
+    width: 4.4em;
+    margin-left: 1rem;
+    background-color: var(--xmas-green);
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    text-transform: uppercase;
+    color: #eee;
+    font-family: 'Dancing Script', cursive;
+    font-size: 1em;
+    letter-spacing: .2rem;
+
+    &:hover {
+      background-color: var(--xmas-dark-green);
     }
+  }
+
+  .outer {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .inner {
+    display: flex;
+    align-items: stretch;
+    justify-content: stretch;
+  }
+
+  label {
+    font-family: 'Dancing Script', cursive;
+    color: var(--xmas-red);
+    font-size: 3rem;
+    @media screen and (min-width: 768px) {
+      font-size: 7rem;
+    }
+    margin-bottom: 2rem;
+  }
 </style>
