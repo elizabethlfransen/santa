1444b05ebda2a7b15c6540f9280c8ffbe9ad719c
Liz Fransen <lfransen@sourceallies.com>
2023-11-02 10:22:02 -0500

    add entries

--- src/routes/[name]/+page.js
+++ src/routes/[name]/+page.js
@@ -1,5 +1,11 @@
+import {NAMES} from "$lib/constants";
+
 export function load({params}) {
     return {
         name: params.name
     }
+}
+
+export function entries() {
+    return NAMES.map(name => ({name}));
 }
